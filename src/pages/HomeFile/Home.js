1675b0f102192483545010a4a47f3bfd6cc39f8f
FredericGuerin <fguerin555@gmail.com>
2025-02-21 19:22:17 +0100

    vendredi19h22

--- src/pages/HomeFile/Home.js
+++ src/pages/HomeFile/Home.js
@@ -6,10 +6,151 @@ const Home = () => {
   return (
     <div>
       <div className={styles.HomePage}>
-        <h1>Frederic Guerin Music composer</h1>
+        <p>Frederic Guerin - Music Composer - fguerin555@gmail.com</p>
+        <p>
+          Born in Boulogne-Billancourt France on 13 November 1959 - Living in
+          Tuscany
+        </p>
+        <p>Studies: Classical high school</p>
+        <p>
+          1992/1995 : School of Composition, Harmony and Counterpoint Jean
+          Robert. Paris.
+        </p>
+        <p>1977: Creation of the rock band “Cholera”. Caen. Normandy.</p>
+        <p>
+          1979: Creation and composition of a 120 minute musical work “SAGA”.
+          Paris.
+        </p>
+        <p>
+          1981: Creation of the rock band “Les Singes Electriques” Caen.
+          Normandy.
+        </p>
+        <p>
+          1982: Creation of the french rock band “Cirage Bleu”. Record company
+          “TREMA”. Paris.
+        </p>
+        <p>SOUNDTRACKS</p>
+        <p>
+          1991: Medium-length film “DRADOUR” 26 minutes by David Martin.
+          <br />
+          Production: LE GAYANT FILMS Paris.
+        </p>
+        <p>
+          1992: Short film “SCHWARTZ” 6 minutes by Frederic Journet.
+          <br />
+          Production: QUARTIER LATIN Paris. CANAL+ diffusion.
+        </p>
+
+        <p>
+          1993: Short film “JE TE NEM” 14 minutes by Frederic Journet and
+          Bernard Schouckroun.
+          <br />
+          Production: QUARTIER LATIN Paris. CANAL+ diffusion.
+        </p>
+        <p>
+          1994: SHORTS TV “BANDES ANNONCES 1” 7 X 2 minutes by Frederic Journet
+          and Bernard Schouckroun.
+          <br />
+          Production: CANAL+ Paris. CANAL+ diffusion
+        </p>
+        <p>
+          1995: Medium-length film “COEURD’AIGUILLE” 24 minutes by Thierry
+          Sebban
+          <br />
+          Production: TWILIGHT ZONE Paris. Broadcast FRANCE 3 TV.
+        </p>
+
+        <p>
+          1995: Documentary “LES RENDEZ-VOUS CAMARGUAIS” 7 x 26 minutes by Paul
+          Carpita.
+          <br />
+          Production QUARTIER LATIN Paris. Broadcast FRANCE 3 TV.
+        </p>
+
+        <p>
+          1995: SHORTS TV “BANDES ANNONCES 2” 7 X 2 minutes by Frederic Journet
+          and Bernard Schouckroun
+          <br />
+          Production: CANAL+ Paris. CANAL+ diffusion.
+        </p>
+        <p>
+          1998: Short film “TOURNIQUET ORANGE” 16 minutes by Thierry Sebban
+          <br />
+          Production: TWILIGHT ZONE Paris. Broadcast FRANCE 3 TV.
+        </p>
+
+        <p>
+          2000: Feature film “INNOCENT POUR CENT” 96 minutes by Bernard
+          Schoukroun and Frederic Journet.
+          <br />
+          Production MAGNIFICAT FILMS Paris. CANAL+ / PARIS PREMIÈRE broadcast.
+        </p>
+
+        <p>
+          2000 to 2004: Collaboration with the television writer and director
+          Roberto Giaccobo
+        </p>
+        <p>Documentaries “GEO & GEO”. RAI 3 Rome.</p>
+
+        <p>
+          2001/2003: RAISAT FICTION Conception and composition of all the
+          original music.
+          <br />
+          Themes/Jingles/Advertisements/Sub- funds/Columns/Promos/Interviews
+          <br />
+          for the satellite channel RAISAT FICTION. Rome.
+        </p>
+        <p>
+          2002: Documentary “VACCARO'S ITALIAN PASTRY SHOP II” 26 minutes by
+          Alessandra Tantillo. Rome
+        </p>
+
+        <p>2010: Documentary music</p>
+
+        <p>
+          “FASCISM AND THE CULT OF THE DUCE”
+          <br />
+          42 minutes by Alessandra Tantillo and Vanessa Roghi.
+          <br />
+          Production / Edition / Diffusion:ARTS and HUMANITY RESEARCH COUNCIL.
+          London.
+        </p>
+
+        <p>
+          “PREDAPPIO: PAST AND PRESENT IN MUSSOLINI’S BIRTHPLACE”
+          <br /> 42 minutes by Alessandra Tantillo and Vanessa Roghi.
+          <br />
+          Production / Edition / Diffusion:ARTS and HUMANITY RESEARCH COUNCIL.
+          London.
+        </p>
+
+        <p>
+          “MUSSOLINI AFTER MUSSOLINI”
+          <br />
+          42 minutes by Alessandra Tantillo and Vanessa Roghi.
+          <br />
+          Production / Edition / Diffusion:ARTS and HUMANITY RESEARCH COUNCIL.
+          London.
+        </p>
+        <p>Musics Documentaries for "La Grande Storia" RAI 3. Rome.</p>
+        <p>
+          2009 to 2012: Creation of songs in French
+          <br />
+          Interpretations in various venues in Rome. “LE GUERIN & LES ITALIENS”.
+        </p>
+        <button className={styles.click1}></button>
+        <p>
+          2011: Teacher of French song at the Instituto Saint-Louis des
+          Français. Rome .
+        </p>
+        <p>2012: Two videos and music for the "Infinite Museum".</p>
+        <button className={styles.click2}></button>
+        <p>2023: Video and music for the association "Ciel'in Città".</p>
+        <button className={styles.click3}></button>
+        <p>2012/2024: Music for documentaries "La Grand Storia" Rai3. Rome.</p>
       </div>
     </div>
   );
 };
 
-export default Home; // Default export
+export default Home;

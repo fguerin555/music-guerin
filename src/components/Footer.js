54e3291064c55a8a4cc75803fdb05dd4509d5284
FredericGuerin <fguerin555@gmail.com>
2025-02-25 17:49:50 +0100

    mardi17h48

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,23 +12,22 @@ const Footer = () => {
       <div className={styles.Footer}>
         <div className={styles.FooterContactInfo}>
           <span className={styles.FooterEmail}>
-            {location.pathname === "/music-guerin" ||
-            location.pathname === "/" ? (
+            {location.pathname !== "/music-guerin" &&
+            location.pathname !== "/" ? (
               <div>
-                <p>All Rights Reserved for All Countries</p>
-                <p>©Frédéric Guérin</p>
-                <p>Società Italiana Autori ed Editori S.I.A.E.</p>
+                <div className={styles.DoYouNeed}>
+                  <p>Do you want to use one of these works?</p>
+                  <p>Do you need originals scores?</p>
+                  <p>Contact me at :</p>{" "}
+                </div>
+                <a href="mailto:fguerin555@gmail.com">fguerin555@gmail.com</a>
               </div>
             ) : (
               <div>
                 <p>All Rights Reserved for All Countries</p>
                 <p>©Frédéric Guérin</p>
                 <p>Società Italiana Autori ed Editori S.I.A.E.</p>
-                <div className={styles.DoYouNeed}>
-                  <p>Do you want to use one of these works?</p>
-                  <p>Do you need originals scores?</p>
-                  <p>Contact me at :</p>{" "}
-                </div>
+
                 <a href="mailto:fguerin555@gmail.com">fguerin555@gmail.com</a>
               </div>
             )}
